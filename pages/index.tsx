7056d1b7532085072da18ee1d0541aa62af11f83
Tushar Patil <manjushakarpe1982@gmail.com>
2024-03-30 12:51:58 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -263,7 +263,7 @@ export default function Home({
                       scrollThreshold={0.2}
                     >
                       <div
-                        className={`grid gap-x-2 gap-y-4 md:gap-y-4 ${
+                        className={`grid gap-x-2 gap-y-4 md:gap-y-4 min-h-[200px] ${
                           view === 'grid'
                             ? 'grid-cols-2 xl:grid-cols-4 '
                             : 'grid-cols-1 lg:grid-cols-2'
